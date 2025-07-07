60605db3b082bd9c1660d853a4d30670cf59ee37
GuilhermeFacu <Guilherme_faculdade_6361@hotmail.com>
2025-07-07 12:53:12 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let botao = document.querySelector("#botao");
+botao.style.background="blue";
+let clicado=false;
+botao.addEventListener("mouseover",e =>{
+    if(!clicado)
+    botao.style.background="rgb(6, 236, 140)";
+});
+
+botao.addEventListener("mouseout",e =>{
+    if(!clicado)
+    botao.style.background="blue";
+});
+
+botao.addEventListener("click",e =>{
+    botao.style.background="black";
+    botao.innerHTML="TUDO SEI QUE NADA SEI";
+    clicado=true
+});
+
+/*let trocafundo = document.querySelector("#image")
+trocafundo.addEventListener("mouseover",e =>{
+    trocafundo.style.background=japão
+
+})
+
+let japão = document.querySelector("#image")
+let islandia = document.querySelector("#image")
+let espanha = document.querySelector("#image") */
